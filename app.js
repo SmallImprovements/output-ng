799efac5ed3a2e0638dd23da9a89617b7cc52415
Sebastian Fröstl <sebastian@froestl.com>
2014-07-01 17:31:45 +0200

    add data, basic search and favorite feature

--- app.js
+++ app.js
@@ -1,14 +1,52 @@
 (function(sandbox) {
     'use strict';
 
-    sandbox.angular.module('app', [])
-    .controller('AppController', AppController);
+    var module = sandbox.angular.module('app', []);
 
-    function AppController($scope) {
-        $scope.movies = [
-            {name: 'From Dusk till Dawn'},
-        ]
-    }
+    module.controller('AppController', function ($scope, $http) {
 
+        $http.get('data.json').success(function (contributions) {
+            $scope.contributions = contributions;
+        });
+
+        $scope.showDetailsFor = {};
+        $scope.order = {};
+
+        $scope.star = function (contribution) {
+            $scope.favorites.push(contribution);
+            contribution.isFavorite = true;
+        };
+
+        $scope.unstar = function (contribution) {
+            $scope.favorites.splice($scope.favorites.indexOf(contribution), 1);
+            contribution.isFavorite = false;
+        }
+
+        $scope.toggleDetails = function (contrib) {
+            $scope.showDetailsFor[contrib.href] = !$scope.showDetailsFor[contrib.href]
+        };
+
+        $scope.orderBy = function (property) {
+            $scope.order.property = property;
+            $scope.order.reverse = !$scope.order.reverse;
+        };
+
+        // sort
+
+        $scope.favorites = [];
+
+    });
+
+
+
+
+    module.directive('contribution', function () {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/contribution-tpl.html',
+            link : function (scope, el, attrs) {
+            }
+        };
+    });
 
 }(this));
